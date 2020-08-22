3c0c774845a8bf484fdd7a4e67cda9ee60299821
William Ritson <william.ritson@gmail.com>
2020-08-22 16:09:50 -0400

    Implements additional attacks.
    Implements Paw, Lick, Butt-hole Exposure and Furball

---
+++ Assets/Scripts/Cat/LaunchProjectileInAllLanesCatAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class LaunchProjectileInAllLanesCatAction : CatAction
+{
+    public GameObject projectile;
+    public float timeToPerform;
+
+    private Cat cat;
+
+    private void Start()
+    {
+        cat = GetComponent<Cat>();
+    }
+
+    public override IEnumerator Perform(Transform target, float speed)
+    {
+        yield return new WaitForSeconds(timeToPerform / 2 /speed);
+        foreach (var lane in cat.movementPoints)
+        {
+            var targetPos = new Vector3(target.position.x, lane.position.y, lane.position.z);
+            Instantiate(projectile, targetPos, target.rotation);
+        }
+        yield return new WaitForSeconds(timeToPerform / 2 /speed);
+    }
+}
