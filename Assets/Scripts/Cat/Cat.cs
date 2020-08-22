d4c823f2a97736bd15903a10e1e1d1952763f21a
Donkid33 <35999999+Donkid33@users.noreply.github.com>
2020-08-22 14:29:50 -0400

    Made increased cat speed actually work
    title

--- Assets/Scripts/Cat/Cat.cs
+++ Assets/Scripts/Cat/Cat.cs
@@ -13,11 +13,21 @@ public class Cat : MonoBehaviour
     private Vector3 movingTowards;
     private AudioSource audioSource;
 
+    private Damageable damageScript;
+
     private CatAction[] actions;
     private bool takeingAction = false;
     private float StunnedDuration = 0;
     private float HyperDuration = 0;
     public float hyperMultiplier = 2;
+
+    private enum State {
+        Neutral,
+        Hyper,
+        Stunned
+        }
+
+    private State catState = State.Neutral;
 
 
     private void Start()
@@ -31,50 +41,41 @@ public class Cat : MonoBehaviour
     {
         // If we are doing somethign don't move
         if (takeingAction) return;
-        
-        if (StunnedDuration <=0 && HyperDuration <=0)
+
+        if (HyperDuration <= 0 && StunnedDuration <= 0)
         {
-            // Move towards the next location
-            var position = transform.position;
-            Vector3 towardsTarget = (movingTowards - position).normalized * (Time.deltaTime * speed);
-            transform.position = position + towardsTarget;
-
-            // Exit if we are not close enough to the destination
-            if (Vector3.Distance(transform.position, movingTowards) > 0.1) return;
-
-            // When we reach a destination stop moving and take an action
-            movingTowards = GetNextMovementLoc();
-            StartCoroutine(TakeAction(1));
+            catState = State.Neutral;
         }
 
         if (StunnedDuration > 0)
         {
+            catState = State.Stunned;
             StunnedDuration -= Time.deltaTime;
         }
 
         if (HyperDuration > 0)
         {
-            //TODO: Lose energy over time.
-            HyperDuration -= Time.deltaTime;
-
-            if (StunnedDuration < 0)
+            if (catState != State.Stunned)
             {
-                // Move towards the next location, affected by Hyper Speed
-                var position = transform.position;
-                Vector3 towardsTarget = (movingTowards - position).normalized * (Time.deltaTime * speed * hyperMultiplier);
-                transform.position = position + towardsTarget;
-
-                // Exit if we are not close enough to the destination
-                if (Vector3.Distance(transform.position, movingTowards) > 0.1) return;
-
-                // When we reach a destination stop moving and take an action
-                movingTowards = GetNextMovementLoc();
-                StartCoroutine(TakeAction(hyperMultiplier));
+                catState = State.Hyper;
             }
-            
+            HyperDuration -= Time.deltaTime;
+        }
+
+
+
+        if (catState == State.Neutral)
+        {
+            Move(3);
+        }
+
+        if (catState == State.Hyper)
+        {
+            Move(hyperMultiplier);   
         } 
 
     }
+
 
     private IEnumerator TakeAction(float CastRate)
     {
@@ -86,7 +87,7 @@ public class Cat : MonoBehaviour
             audioSource.Play();
         }
         takeingAction = true;
-        yield return toPerform.Perform(firePoint, 1);
+        yield return toPerform.Perform(firePoint, CastRate);
         takeingAction = false;
     }
 
@@ -100,5 +101,20 @@ public class Cat : MonoBehaviour
 
         nextMovementPoint += moveDir;
         return movementPoints[nextMovementPoint].position;
+    }
+
+    private void Move(float multiplier)
+    {
+        // Move towards the next location
+        var position = transform.position;
+        Vector3 towardsTarget = (movingTowards - position).normalized * (Time.deltaTime * speed * multiplier);
+        transform.position = position + towardsTarget;
+
+        // Exit if we are not close enough to the destination
+        if (Vector3.Distance(transform.position, movingTowards) > 0.1) return;
+
+        // When we reach a destination stop moving and take an action
+        movingTowards = GetNextMovementLoc();
+        StartCoroutine(TakeAction(multiplier));
     }
 }
