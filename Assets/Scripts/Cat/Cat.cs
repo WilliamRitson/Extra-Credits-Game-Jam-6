9186f291c6a908df7090ef9bd9ae1e48a37931cc
William Ritson <william.ritson@gmail.com>
2020-08-21 23:44:15 -0400

    Adds mew attack.

--- Assets/Scripts/Cat/Cat.cs
+++ Assets/Scripts/Cat/Cat.cs
@@ -7,47 +7,52 @@ public class Cat : MonoBehaviour
 {
     public Transform firePoint;
     public Transform[] movementPoints;
-    public float baseSpeed = 2;
-    private float currentSpeed;
+    public float speed = 2;
     private int nextMovementPoint = 0;
     private int moveDir = 1;
     private Vector3 movingTowards;
     private AudioSource audioSource;
 
     private CatAction[] actions;
+    private bool takeingAction = false;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         actions = GetComponents<CatAction>();
-        currentSpeed = baseSpeed;
         movingTowards = movementPoints[0].position;
     }
 
     private void Update()
     {
+        // If we are doing somethign don't move
+        if (takeingAction) return;
+        
         // Move towards the next location
         var position = transform.position;
-        Vector3 towardsTarget = (movingTowards - position).normalized * (Time.deltaTime * currentSpeed);
+        Vector3 towardsTarget = (movingTowards - position).normalized * (Time.deltaTime * speed);
         transform.position = position + towardsTarget;
         
+        // Exit if we are not close enough to the destination
+        if (Vector3.Distance(transform.position, movingTowards) > 0.1) return;
+        
         // When we reach a destination stop moving and take an action
-        if (!(Vector3.Distance(transform.position, movingTowards) < 1)) return;
         movingTowards = GetNextMovementLoc();
-        currentSpeed = 0;
         StartCoroutine(TakeAction());
     }
 
     private IEnumerator TakeAction()
     {
         CatAction toPerform = actions[Random.Range(0, actions.Length)];
+        Debug.Log("Use action: " + toPerform.abilityTitle);
         if (toPerform.soundEffect != null)
         {
             audioSource.clip = toPerform.soundEffect;
             audioSource.Play();
         }
+        takeingAction = true;
         yield return toPerform.Perform(firePoint);
-        currentSpeed = baseSpeed;
+        takeingAction = false;
     }
 
     private Vector3 GetNextMovementLoc()
--- Assets/Scripts/Cat/CatAction.cs
+++ Assets/Scripts/Cat/CatAction.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public abstract class CatAction : MonoBehaviour
 {
+    public string abilityTitle;
     public AudioClip soundEffect;
     public abstract IEnumerator Perform(Transform target);
 }
