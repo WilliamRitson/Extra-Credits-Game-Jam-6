5e22723282cd9637d6c1275e873e4d60e064404e
Alexis Abel <4960608+LexyVFX@users.noreply.github.com>
2020-08-24 11:48:09 -0600

    Added object spawning script for damage events

---
+++ Assets/Scripts/Damage/SpawnObjectOnDamage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class SpawnObjectOnDamage : OnDamageBehavior
+{
+    [SerializeField] GameObject toSpawn;
+
+    protected override void OnDamaged(int damage)
+    {
+        if (damage == 0) return;;
+        Instantiate(toSpawn, transform.position, transform.rotation);
+    }
+}
